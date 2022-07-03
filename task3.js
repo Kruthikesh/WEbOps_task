4ad4c57078dcbd03a08028ceb776a42386d349b7
Kruthikesh <98465500+Kruthikesh@users.noreply.github.com>
2022-07-03 08:40:00 +0530

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,7 @@
+
+document.getElementById('btnradio1').onclick = function() {
+   document.getElementById("heading").innerHTML = "You've clicked button-1";
+};
+document.getElementById('btnradio2').onclick = function() {
+   document.getElementById("heading").innerHTML = "You've clicked button-2";
+};
